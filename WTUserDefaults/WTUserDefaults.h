68c2a97d3981ab292244486b7345526faa2e3aad
hgsong <hgsong@wantedlab.com>
2017-07-12 12:28:16 +0900

    Initial Commit

---
+++ WTUserDefaults/WTUserDefaults.h
@@ -0,0 +1,19 @@
+//
+//  WTUserDefaults.h
+//  WTUserDefaults
+//
+//  Created by Ross on 2017. 7. 12..
+//  Copyright © 2017년 wanted. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for WTUserDefaults.
+FOUNDATION_EXPORT double WTUserDefaultsVersionNumber;
+
+//! Project version string for WTUserDefaults.
+FOUNDATION_EXPORT const unsigned char WTUserDefaultsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <WTUserDefaults/PublicHeader.h>
+
+
