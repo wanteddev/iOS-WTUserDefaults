0db0529117da9df9fadb6ffe1768eb83e6c3f056
hgsong <hgsong@wantedlab.com>
2017-07-13 10:24:45 +0900

    init

---
+++ WTUserDefaults/WTUserDefaultsHelper.swift
@@ -0,0 +1,46 @@
+//
+//  UserDefaultsHelper.swift
+//  wanted
+//
+//  Created by Ross on 2017. 6. 20..
+//  Copyright © 2017년 WantedLab. All rights reserved.
+//
+
+import Foundation
+
+public class WTUserDefaultsHelper : WTUserDefaultsProtocol {
+    var userDefaults: UserDefaults
+    
+    required public init() {
+        self.userDefaults = UserDefaults()
+    }
+    
+    required public init(suiteName: String) {
+        if let defaults = UserDefaults(suiteName: suiteName) {
+            self.userDefaults = defaults
+        }
+        else {
+            self.userDefaults = UserDefaults.standard
+        }
+    }
+    
+    public func getBool(forKey:String) -> Bool {
+        return userDefaults.bool(forKey:forKey)
+    }
+    
+    public func getInteger(forKey:String) -> Int {
+        return userDefaults.integer(forKey:forKey)
+    }
+    
+    public func getObject(forKey:String) -> Any? {
+        return userDefaults.object(forKey:forKey)
+    }
+    
+    public func array(forKey:String) -> [Any]? {
+        return userDefaults.array(forKey: forKey)
+    }
+    
+    public func set(_ value:Any, forKey:String) {
+        userDefaults.set(value, forKey:forKey)
+    }
+}
---
+++ WTUserDefaults/WTUserDefaultsProtocol.swift
@@ -0,0 +1,19 @@
+//
+//  UserDefaultsProtocol.swift
+//  wanted
+//
+//  Created by Ross on 2017. 6. 20..
+//  Copyright © 2017년 WantedLab. All rights reserved.
+//
+
+import Foundation
+
+public protocol WTUserDefaultsProtocol {
+    init()
+    init(suiteName: String)
+    func getBool(forKey: String) -> Bool
+    func getInteger(forKey: String) -> Int
+    func getObject(forKey: String) -> Any?
+    func array(forKey: String) -> [Any]?
+    func set(_ value:Any, forKey: String)
+}
